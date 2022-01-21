ce5e62a52b78b010fc5463fb126f08dd0e8b67ef
soulwish <i4ya@ya.ru>
2022-01-22 00:34:43 +0300

    simplification of code

--- cli/gen/generator.go
+++ cli/gen/generator.go
@@ -22,6 +22,10 @@ func (self *clitype) addfn(fn clifuncs) {
 	self.funcs = append(self.funcs, fn)
 }
 
+func (self *clitype) start_func() string {
+	return "func (self *"+self.typename+") "
+}
+
 func (self *clitype) call_var() string {
 	return self.typename + "CliStruct"
 }
@@ -76,7 +80,7 @@ func (self *cliGenerator) checkDecls(root *ast.File) bool {
 		if id, ok = se.X.(*ast.Ident); !ok { continue }
 		if len(fd.Type.Results.List) != 1 { continue }
 		if rt, ok = fd.Type.Results.List[0].Type.(*ast.Ident); !ok || rt.Name != "error" { continue }
-		if !strings.HasPrefix(fd.Name.Name, "Cli") { continue }
+		if !strings.HasPrefix(fd.Name.Name, "CliCmd") { continue }
 
 		tn = id.Name
 
@@ -89,7 +93,7 @@ func (self *cliGenerator) checkDecls(root *ast.File) bool {
 
 		hw = true
 
-		cmd = strings.ToLower(strings.TrimPrefix(fd.Name.Name, "Cli"))
+		cmd = strings.ToLower(strings.TrimPrefix(fd.Name.Name, "CliCmd"))
 
 		clf := clifuncs{
 			name:     fd.Name.Name,
@@ -121,12 +125,6 @@ func (self *cliGenerator) checkDecls(root *ast.File) bool {
 	return hw
 }
 
-
-func (self *cliGenerator) genCliTop(fw *os.File, t *clitype) {
-	fw.WriteString("type "+t.typename+"CliCmdFunc func([]string) error\n\n")
-	fw.WriteString("var "+t.call_var()+" "+t.typename+"\n\n")
-}
-
 func (self *cliGenerator) genCliHelp(fw *os.File, t *clitype) {
 	fw.WriteString("func " + t.call_help() + "() {\n\t")
 
@@ -139,91 +137,65 @@ func (self *cliGenerator) genCliHelp(fw *os.File, t *clitype) {
 		fw.WriteString("\")\n\t")
 	}
 
-	fw.WriteString("fmt.Println(\"\")\n")
-
-	fw.WriteString("}\n\n")
-}
-
-func (self *cliGenerator) genCliCallerFunc(fw *os.File, t *clitype, fn *clifuncs) {
-	var (
-		parr []string
-		iserr bool
-	)
-	fw.WriteString("func " + fn.call_name() + "(s_args []string) error {\n\t")
-	
-	for i, p := range fn.params {
-		if p.gotype == "string" {
-			parr = append(parr, fmt.Sprintf("s_args[%d]", i))
-		} else {
-			iserr = true
-			switch p.gotype {
-			case "int8", "int16", "int32", "int64":
-				fw.WriteString(fmt.Sprintf("var p%d int64\n\t", i+1))
-			case "uint8", "uint16", "uint32", "uint64":
-				fw.WriteString(fmt.Sprintf("var p%d uint64\n\t", i+1))
-			case "float32", "float64":
-				fw.WriteString(fmt.Sprintf("var p%d float64\n\t", i+1))
-			case "bool":
-				fw.WriteString(fmt.Sprintf("var p%d bool\n\t", i+1))
-			}
-		}
-	}
-
-	if iserr {
-		fw.WriteString("var err error\n\t")
-	}
-
-	fw.WriteString(fmt.Sprintf("if len(s_args) != %d {\n\t\t", len(fn.params)))
-	fw.WriteString("fmt.Println(\"Error: incorrect number of parameters\")\n\t\t")
-	fw.WriteString("fmt.Println(\"Example: "+fn.example_cmd() + "\")\n\t}\n\t")
-
-	for i, p := range fn.params {
-		if p.gotype == "string" { continue }
-
-		switch p.gotype {
-		case "int8", "int16", "int32", "int64":
-			parr = append(parr, fmt.Sprintf(p.gotype+"(p%d)", i+1))
-			fw.WriteString(fmt.Sprintf("p%d, err = strconv.ParseInt(s_args[%d], 10, %s)\n\t", i+1, i, strings.TrimPrefix(p.gotype, "int")))
-		case "uint8", "uint16", "uint32", "uint64":
-			parr = append(parr, fmt.Sprintf(p.gotype+"(p%d)", i+1))
-			fw.WriteString(fmt.Sprintf("p%d, err = strconv.ParseUint(s_args[%d], 10, %s)\n\t", i+1, i, strings.TrimPrefix(p.gotype, "uint")))
-		case "float32", "float64":
-			parr = append(parr, fmt.Sprintf(p.gotype+"(p%d)", i+1))
-			fw.WriteString(fmt.Sprintf("p%d, err = strconv.ParseFloat(s_args[%d], %s)\n\t", i+1, i, strings.TrimPrefix(p.gotype, "float")))
-		case "bool":
-			parr = append(parr, fmt.Sprintf("p%d", i+1))
-			fw.WriteString(fmt.Sprintf("p%d, err = strconv.ParseBool(s_args[%d])\n\t", i+1, i))
-		}
-		fw.WriteString("if err != nil {\n\t\t")
-		fw.WriteString("fmt.Println(\"Error:\", err)\n\t\t")
-		fw.WriteString("fmt.Println(\"Example: "+fn.example_cmd() + "\")\n\t\t")
-		fw.WriteString("return err\n\t")
-		fw.WriteString("}\n\t")
-	}
-
-	fw.WriteString("return " + t.call_var() + "." + fn.name + "("+strings.Join(parr, ", ")+")\n")
+	fw.WriteString("fmt.Println(\"\")\n\t")
+	fw.WriteString("strconv.FormatBool(true)\n")
 
 	fw.WriteString("}\n\n")
 }
 
 func (self *cliGenerator) genCliCaller(fw *os.File, t *clitype) {
-	var (
-		mapvars []string
-	)
 
+	fw.WriteString(t.start_func() + "CliRun(s_args []string) error {\n\t")
+	fw.WriteString("if len(s_args) == 0 || s_args[0] == \"?\" {\n\t\t"+t.call_help()+"()\n\t\treturn nil\n\t}\n\t")
+
+	fw.WriteString("s_args_cmd := s_args[0]\n\t")
+	fw.WriteString("s_args = s_args[1:]\n\t")
+	fw.WriteString("switch hash.MurMur2([]byte(s_args_cmd)) {\n\t")
 	for _, fn := range t.funcs {
-		mapvars = append(mapvars, fmt.Sprintf("%d: %s", fn.cmdhash, fn.call_name()))
-		self.genCliCallerFunc(fw, t, &fn)
+		var parr []string
+		fw.WriteString(fmt.Sprintf("case %d:\n\t\t", fn.cmdhash))
+
+		fw.WriteString(fmt.Sprintf("if len(s_args) != %d {\n\t\t\t", len(fn.params)))
+		fw.WriteString("fmt.Println(\"Error: incorrect number of parameters\")\n\t\t\t")
+		fw.WriteString("fmt.Println(\"Example: "+fn.example_cmd() + "\")\n\t\t}\n\t\t")
+
+		for i, p := range fn.params {
+			var iserr = false
+			if p.gotype == "string" {
+				parr = append(parr, fmt.Sprintf("s_args[%d]", i))
+			} else {
+				iserr = true
+				switch p.gotype {
+				case "int8", "int16", "int32", "int64":
+					fw.WriteString(fmt.Sprintf("p%d, err := strconv.ParseInt(s_args[%d], 10, %s)\n\t\t", i+1, i, strings.TrimPrefix(p.gotype, "int")))
+					parr = append(parr, fmt.Sprintf(p.gotype+"(p%d)", i+1))
+				case "uint8", "uint16", "uint32", "uint64":
+					fw.WriteString(fmt.Sprintf("p%d, err := strconv.ParseUint(s_args[%d], 10, %s)\n\t\t", i+1, i, strings.TrimPrefix(p.gotype, "uint")))
+					parr = append(parr, fmt.Sprintf(p.gotype+"(p%d)", i+1))
+				case "float32", "float64":
+					fw.WriteString(fmt.Sprintf("p%d, err := strconv.ParseFloat(s_args[%d], %s)\n\t\t", i+1, i, strings.TrimPrefix(p.gotype, "float")))
+					parr = append(parr, fmt.Sprintf(p.gotype+"(p%d)", i+1))
+				case "bool":
+					fw.WriteString(fmt.Sprintf("p%d, err := strconv.ParseBool(s_args[%d])\n\t\t", i+1, i))
+					parr = append(parr, fmt.Sprintf(p.gotype+"(p%d)", i+1))
+				}
+			}
+
+			if iserr {
+				fw.WriteString("if err != nil {\n\t\t\t")
+				fw.WriteString("fmt.Println(\"CLI error `"+p.name+"` parameter must be "+p.gotype+"\")\n\t\t\t")
+				fw.WriteString("return err\n\t\t")
+				fw.WriteString("}\n\t\t")
+			}
+		}
+
+		fw.WriteString("return " + "self." + fn.name + "("+strings.Join(parr, ", ")+")\n\t")
 	}
 
-	fw.WriteString("func " + t.typename + "CliRun(s_args []string) error {\n\t")
-	fw.WriteString("var cmd_hash uint32\n\t")
-	fw.WriteString("var cmd_map = map[uint32]TAppBrendCliCmdFunc{\n\t\t"+strings.Join(mapvars, ",\n\t\t")+",\n\t}\n\t")
-	fw.WriteString("if len(s_args) == 0 || s_args[0] == \"?\" {\n\t\t"+t.call_help()+"()\n\t\treturn nil\n\t}\n\t")
-	fw.WriteString("cmd_hash = hash.MurMur2([]byte(s_args[0]))\n\t")
-	fw.WriteString("if fn, ok := cmd_map[cmd_hash]; ok { return fn(s_args[1:]) }\n\t")
-	fw.WriteString("fmt.Println(`Error: command \"`+s_args[0]+`\" not found`)\n\t")
+	fw.WriteString("default:\n\t\t")
+	fw.WriteString("fmt.Println(`Error: command \"`+s_args_cmd+`\" not found`)\n\t\t")
 	fw.WriteString(t.call_help() + "()\n\t")
+	fw.WriteString("}\n\t")
 	fw.WriteString("return nil\n")
 	fw.WriteString("}")
 }
@@ -251,7 +223,6 @@ func Generate(root *ast.File, f *os.File) bool {
 	f.WriteString(")\n\n")
 
 	for _, t := range gen.ctypes {
-		gen.genCliTop(f, t)
 		gen.genCliHelp(f, t)
 		gen.genCliCaller(f, t)
 	}
