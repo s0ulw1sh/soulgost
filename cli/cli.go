d6cea62d1172b3c82e56aa17807bbba0132cc327
soulwish <i4ya@ya.ru>
2022-01-30 18:15:07 +0300

    rewrite cli

--- cli/cli.go
+++ cli/cli.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"log"
 	"fmt"
 )
 
@@ -17,21 +18,21 @@ func Register(name string, cli CliType) {
 }
 
 func Run(args_s []string) {
-	if len(args_s) < 2 {
-		return
-	}
-
-	if cli, ok := clis[args_s[0]]; ok {
-		cli.CliRun(args_s[1:])
-		return
-	}
-
 	if len(clis) > 0 {
-		fmt.Println("command not found, available commands:")
-
-		for name, _ := range clis {
-			fmt.Println("\t" + name)
+		if cli, ok := clis[args_s[0]]; ok && len(args_s) >= 2 {
+			err := cli.CliRun(args_s[1:])
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			fmt.Println("command not found, available commands:")
+			for name, _ := range clis {
+				fmt.Println("\t" + name)
+			}
 		}
+	} else {
+		fmt.Println("invalid command line params")
 	}
 
+	os.Exit(0)
 }
