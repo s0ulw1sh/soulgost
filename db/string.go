be6fbb3f94291784f11416dce0ec17e8b1968e54
soulwish <i4ya@ya.ru>
2022-02-18 21:09:28 +0300

    fix naming

--- db/string.go
+++ db/string.go
@@ -3,10 +3,11 @@ package db
 import (
 	"database/sql"
 	"encoding/json"
+	"database/sql/driver"
 )
 
 type StrNull struct {
-	sql.NullString
+	ns sql.NullString
 }
 
 func StrNullNew(str string) StrNull {
@@ -16,70 +17,86 @@ func StrNullNew(str string) StrNull {
 }
 
 func (s StrNull) String() string {
-	if !s.Valid {
+	if !s.ns.Valid {
 		return ""
 	}
-	return s.String
+	return s.ns.String
+}
+
+func (s *StrNull) Scan(value interface{}) error {
+	return s.ns.Scan(value)
+}
+
+func (s StrNull) Value() (driver.Value, error) {
+	return s.ns.Value()
 }
 
 func (s StrNull) MarshalJSON() ([]byte, error) {
-	if !s.Valid {
+	if !s.ns.Valid {
 		return []byte("null"), nil
 	}
-	return json.Marshal(s.String)
+	return json.Marshal(s.ns.String)
 }
 
 func (s *StrNull) UnmarshalJSON(b []byte) error {
-	err := json.Unmarshal(b, &s.String)
-	s.Valid = (err == nil)
+	err := json.Unmarshal(b, &s.ns.String)
+	s.ns.Valid = (err == nil)
 	return err
 }
 
 func (s *StrNull) Set(val string) {
-	s.Valid  = val != ""
-	s.String = val
+	s.ns.Valid  = val != ""
+	s.ns.String = val
 }
 
 func (s *StrNull) Val() string {
-	if s.Valid {
-		return s.String
+	if s.ns.Valid {
+		return s.ns.String
 	} else {
 		return ""
 	}
 }
 
 type StrEmpty struct {
-	sql.NullString
+	ns sql.NullString
+}
+
+func (s *StrEmpty) Scan(value interface{}) error {
+	return s.ns.Scan(value)
+}
+
+func (s StrEmpty) Value() (driver.Value, error) {
+	return s.ns.Value()
 }
 
 func (s StrEmpty) String() string {
-	if !s.Valid {
+	if !s.ns.Valid {
 		return "-"
 	}
-	return s.String
+	return s.ns.String
 }
 
 func (s StrEmpty) MarshalJSON() ([]byte, error) {
-	if !s.Valid {
+	if !s.ns.Valid {
 		return []byte("\"-\""), nil
 	}
-	return json.Marshal(s.String)
+	return json.Marshal(s.ns.String)
 }
 
 func (s *StrEmpty) UnmarshalJSON(b []byte) error {
-	err := json.Unmarshal(b, &s.String)
-	s.Valid = (err == nil)
+	err := json.Unmarshal(b, &s.ns.String)
+	s.ns.Valid = (err == nil)
 	return err
 }
 
 func (s *StrEmpty) Set(val string) {
-	s.Valid  = val != ""
-	s.String = val
+	s.ns.Valid  = val != ""
+	s.ns.String = val
 }
 
 func (s *StrEmpty) Val() string {
-	if s.Valid {
-		return s.String
+	if s.ns.Valid {
+		return s.ns.String
 	} else {
 		return ""
 	}
