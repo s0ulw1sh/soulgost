ba96e24f1ec3ae19ca4bf4e59a061897a7434129
swanxone <i4ya@ya.ru>
2022-04-04 13:13:44 +0300

    add SanitizeWithSpace

--- txt/sanitize.go
+++ txt/sanitize.go
@@ -27,3 +27,28 @@ func Sanitize(text string) string {
 
 	return output.String()
 }
+
+func SanitizeWithSpace(text string) string {
+	var (
+		r   rune
+		err error
+	)
+
+	var input  = bytes.NewBufferString(text)
+	var output = bytes.NewBuffer(nil)
+
+	for {
+		if r, _, err = input.ReadRune(); err != nil {
+			break
+		}
+
+		switch {
+		case IsRuRune(r):  fallthrough
+		case IsEnRune(r):  fallthrough
+		case IsDigitRune(r): fallthrough
+			output.WriteRune(r)
+		}
+	}
+
+	return output.String()
+}
