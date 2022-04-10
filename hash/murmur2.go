964d3c1a263199eaff8c5401972c8a61ad45daf1
swanxone <i4ya@ya.ru>
2022-04-10 17:03:29 +0300

    modify murmur2

--- hash/murmur2.go
+++ hash/murmur2.go
@@ -1,45 +1,97 @@
 package hash
 
-const (
-	murmur2_m_const    = 0x5bd1e995
-	murmur2_seed_const = 0
-)
+type MurMur2Hash struct {
+	m_const uint32
+	hash    uint32
+	lcount  int
+	last    [3]byte
+}
 
-func murmur2_mix(h uint32, k uint32) (uint32, uint32) {
-	k *= murmur2_m_const
+func (self *MurMur2Hash) mix(h uint32, k uint32) (uint32, uint32) {
+	k *= self.m_const
 	k ^= k >> 24
-	k *= murmur2_m_const
-	h *= murmur2_m_const
+	k *= self.m_const
+	h *= self.m_const
 	h ^= k
+
 	return h, k
 }
 
-func MurMur2(data []byte) (h uint32) {
+func (self *MurMur2Hash) Init(seed uint32) {
+	self.m_const = 0x5bd1e995
+	self.hash    = seed
+}
+
+func (self *MurMur2Hash) Update(data []byte) {
 	var k uint32
+	var l int = len(data)
 
-	h = murmur2_seed_const ^ uint32(len(data))
+	if l == 0 { return }
 
-	for l := len(data); l >= 4; l -= 4 {
-		k    = uint32(data[0]) | uint32(data[1])<<8 | uint32(data[2])<<16 | uint32(data[3])<<24
-		h, k = murmur2_mix(h, k)
-		data = data[4:]
+	if self.lcount > 0 && l - self.lcount > 0 {
+		k = 0
+
+		switch self.lcount {
+		case 3:
+			k = uint32(self.last[0]) | uint32(self.last[1])<<8 | uint32(self.last[2])<<16 | uint32(data[0])<<24
+		case 2:
+			k = uint32(self.last[0]) | uint32(self.last[1])<<8 | uint32(data[0])<<16 | uint32(data[1])<<24
+		case 1:
+			k = uint32(self.last[0]) | uint32(data[0])<<8 | uint32(data[1])<<16 | uint32(data[2])<<24
+		}
+
+		l = l - (4-self.lcount)
+		data = data[4-self.lcount:]
+		self.hash, k = self.mix(self.hash, k)
+		self.lcount  = 0
 	}
 
-	switch len(data) {
+	for ; l >= 4; l -= 4 {
+		k            = uint32(data[0]) | uint32(data[1])<<8 | uint32(data[2])<<16 | uint32(data[3])<<24
+		self.hash, k = self.mix(self.hash, k)
+		data         = data[4:]
+	}
+
+	self.lcount = len(data)
+
+	switch self.lcount {
 	case 3:
-		h ^= uint32(data[2]) << 16
+		self.last[2] = data[2]
 		fallthrough
 	case 2:
-		h ^= uint32(data[1]) << 8
+		self.last[1] = data[1]
 		fallthrough
 	case 1:
-		h ^= uint32(data[0])
-		h *= murmur2_m_const
+		self.last[0] = data[0]
+	}
+}
+
+func (self *MurMur2Hash) Finish() uint32 {
+
+	switch self.lcount {
+	case 3:
+		self.hash ^= uint32(self.last[2]) << 16
+		fallthrough
+	case 2:
+		self.hash ^= uint32(self.last[1]) << 8
+		fallthrough
+	case 1:
+		self.hash ^= uint32(self.last[0])
+		self.hash *= self.m_const
 	}
 
-	h ^= h >> 13
-	h *= murmur2_m_const
-	h ^= h >> 15
+	self.hash ^= self.hash >> 13
+	self.hash *= self.m_const
+	self.hash ^= self.hash >> 15
 
-	return
+	return self.hash
+}
+
+func MurMur2(data []byte) uint32 {
+	var h MurMur2Hash
+
+	h.Init(uint32(0 ^ len(data)))
+	h.Update(data)
+
+	return h.Finish()
 }
--- hash/murmur2_test.go
+++ hash/murmur2_test.go
@@ -2,9 +2,27 @@ package hash
 
 import "testing"
 
-func TestMurMur2(t *testing.T) {
+/*func TestMurMur2(t *testing.T) {
 	data := []byte("HelloWorld")
 	hash := MurMur2(data)
+	eq   := uint32(147875502)
+
+	if hash != eq {
+		t.Error("Not equal hashes", hash, eq)
+	}
+}*/
+
+func TestMurMur2Part(t *testing.T) {
+	data1 := []byte("Hello")
+	data2 := []byte("World")
+
+	var h MurMur2Hash
+
+	h.Init(10)
+	h.Update(data1)
+	h.Update(data2)
+
+	hash := h.Finish()
 	eq   := uint32(147875502)
 
 	if hash != eq {
